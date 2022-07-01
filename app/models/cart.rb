9a7a869d7e33a6afdb064bf0e37e11b3e682fcd9
francoriquelme <francoestebanru@gmail.com>
2022-07-01 12:10:35 -0400

    feat: Carts MVC

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,2 +1,30 @@
 class Cart < ApplicationRecord
-end
+    class Car < ApplocationRecord 
+        def payment_method
+            payment_method = PaymentMethod.find_by(code: "PEC")
+            Payment.create(
+                order_id: order.id,
+                payment_method_id: payment_method.id,
+                state: "processing",
+                total: order.total,
+                token: response.token
+            )
+        end
+    
+        def process_paypal_payment
+            response = EXPRESS_GATEWAY.purchase(price, express_purchase_options)
+            if response.success?
+                payment = Payment.find_by(token: response.token)
+                order = payment.order
+    
+                #update object states   
+                payment.state = "completed"
+                order.state = "completed"
+    
+                ActiveRecord::Base.transaction do
+                    order.save!
+                    payment.save!
+                end
+            end
+        end
+    end
